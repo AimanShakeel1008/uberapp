17003450e4f8a20446fd6c74751238ea0eaad6f0
aimanshakeel <aimanshakeel1008@gmail.com>
2024-09-09 00:26:06 +0530

    implementing spring security: login with jwt token

--- src/main/java/com/codingshuttle/project/uber/uberApp/controllers/AuthController.java
+++ src/main/java/com/codingshuttle/project/uber/uberApp/controllers/AuthController.java
@@ -1,10 +1,10 @@
 package com.codingshuttle.project.uber.uberApp.controllers;
 
-import com.codingshuttle.project.uber.uberApp.dto.DriverDto;
-import com.codingshuttle.project.uber.uberApp.dto.OnBoardDriverDto;
-import com.codingshuttle.project.uber.uberApp.dto.SignupDto;
-import com.codingshuttle.project.uber.uberApp.dto.UserDto;
+import com.codingshuttle.project.uber.uberApp.dto.*;
 import com.codingshuttle.project.uber.uberApp.services.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +26,19 @@ public class AuthController {
 	ResponseEntity<DriverDto> onBoardNewDriver(@PathVariable Long userId, @RequestBody OnBoardDriverDto onBoardDriverDto) {
 
 		return new ResponseEntity<>(authService.onBoardNewDriver(userId, onBoardDriverDto.getVehicleId()), HttpStatus.CREATED);
+	}
+
+	@PostMapping("/login")
+	ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse httpServletResponse) {
+
+		String[] tokens = authService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+
+		Cookie cookie = new Cookie("token" , tokens[1]);
+		cookie.setHttpOnly(true);
+
+		httpServletResponse.addCookie(cookie);
+
+		return ResponseEntity.ok(new LoginResponseDto(tokens[0]));
 	}
 }
 
--- src/main/java/com/codingshuttle/project/uber/uberApp/services/impl/AuthServiceImpl.java
+++ src/main/java/com/codingshuttle/project/uber/uberApp/services/impl/AuthServiceImpl.java
@@ -9,12 +9,17 @@ import com.codingshuttle.project.uber.uberApp.entities.enums.Role;
 import com.codingshuttle.project.uber.uberApp.exceptions.ResourceNotFoundException;
 import com.codingshuttle.project.uber.uberApp.exceptions.RuntimeConflictException;
 import com.codingshuttle.project.uber.uberApp.repositories.UserRepository;
+import com.codingshuttle.project.uber.uberApp.security.JWTService;
 import com.codingshuttle.project.uber.uberApp.services.AuthService;
 import com.codingshuttle.project.uber.uberApp.services.DriverService;
 import com.codingshuttle.project.uber.uberApp.services.RiderService;
 import com.codingshuttle.project.uber.uberApp.services.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,9 +36,25 @@ public class AuthServiceImpl implements AuthService {
 	private final WalletService walletService;
 
 	private final DriverService driverService;
+
+	private final PasswordEncoder passwordEncoder;
+
+	private final AuthenticationManager authenticationManager;
+
+	private final JWTService jwtService;
 	@Override
-	public String login(String email, String password) {
-		return null;
+	public String[] login(String email, String password) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(email, password)
+		);
+
+		User user = (User)authentication.getPrincipal();
+
+		String accessToken = jwtService.generateAccessToken(user);
+		String refreshToken = jwtService.generateRefreshToken(user);
+
+		return new String[]{accessToken, refreshToken};
 	}
 
 	@Override
@@ -45,6 +66,8 @@ public class AuthServiceImpl implements AuthService {
 		if (user != null) throw new RuntimeConflictException("Cannot signup, User already exists with email " +signupDto.getEmail());
 
 		User mappedUser = modelMapper.map(signupDto, User.class);
+
+		mappedUser.setPassword(passwordEncoder.encode(mappedUser.getPassword()));
 
 		mappedUser.setRoles(Set.of(Role.RIDER));
 
@@ -70,7 +93,7 @@ public class AuthServiceImpl implements AuthService {
 		Driver createDriver = Driver.builder()
 				.user(user)
 				.rating(0.0)
-				.vehicleId("")
+				.vehicleId(vehicleId)
 				.available(true)
 				.build();
 		user.getRoles().add(Role.DRIVER);
